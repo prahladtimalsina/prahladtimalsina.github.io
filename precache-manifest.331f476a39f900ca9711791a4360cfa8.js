01216af529610539a37a3a2f9553b127a38563ea
prahladtimalsina <prahladtimalsina7@gmail.com>
2021-08-13 12:59:22 +0545

    Updates

---
+++ precache-manifest.331f476a39f900ca9711791a4360cfa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e6337bbb257c5ade66f014d6ae14f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4372e1afefc5912ad37",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "62a08bbb3e7a567c1488",
+    "url": "/static/js/2.284d2a83.chunk.js"
+  },
+  {
+    "revision": "52424457e5c4fa93c31028243ad4df2b",
+    "url": "/static/js/2.284d2a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4372e1afefc5912ad37",
+    "url": "/static/js/main.e386fc91.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
